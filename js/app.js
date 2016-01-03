389741f37a2bbdb164b1fc10ba32261efa5905bd
glenr4 <glenrutherford4@gmail.com>
2016-01-04 05:15:49 +0800

    Processes a guess correctly

--- js/app.js
+++ js/app.js
@@ -8,7 +8,6 @@ $(document).ready(function(){
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
-
   	});
 
   	/*--- Hide information modal box ---*/
@@ -19,23 +18,20 @@ $(document).ready(function(){
   	// Create a new game
   	$(".new").click(function(){
   		newGame();
-  	})
+  	});
 
 	// Reset for a new game
 	function newGame(){
 		// Number to guess
 		numToGuess = Math.round(Math.random()*100);
-
+		console.log(numToGuess);
 		// Reset text and count
 		$("#feedback").empty().append("Make your Guess!");
 		$("#count").empty().append("0");
 	};
 
-	// Create new game on page load
-	newGame();
-
 	// Process a guess
-	$("#guessButton").submit(function(event){
+	$(".game form").submit(function(event){
 		// stop submit from refreshing the page
 		event.preventDefault();
 
@@ -43,24 +39,30 @@ $(document).ready(function(){
 		var diff = Math.abs(guess - numToGuess);
 
 		switch(true){
-			case (diff > 20):
-				$("#feedback").empty().append("Super cold!");
+			case (diff === 0):
+				$("#feedback").empty().append("You guessed right!");
 				break;
-			case (diff <= 20):
-				$("#feedback").empty().append("Cold");
-				break;
-			case (diff <= 10):
-				$("#feedback").empty().append("Warm");
+			case (diff <= 2):
+				$("#feedback").empty().append("Super hot");
 				break;
 			case (diff <= 5):
 				$("#feedback").empty().append("Hot");
 				break;
-			case (diff <= 2):
-				$("#feedback").empty().append("Super hot");
+			case (diff <= 10):
+				$("#feedback").empty().append("Warm");
+				break;
+			case (diff <= 20):
+				$("#feedback").empty().append("Cold");
+				break;
+			case (diff > 20):
+				$("#feedback").empty().append("Super cold!");
 				break;
 			default:
 				$("#feedback").empty().append("Make your Guess!");
 				break;
 		};
 	});
+
+	// Create new game on page load
+	newGame();
 });
