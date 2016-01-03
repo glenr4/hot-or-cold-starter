695271730d7bee78c73f59b36aa549a6ff876706
glenr4 <glenrutherford4@gmail.com>
2016-01-03 14:18:33 +0800

    Debugging

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
 
 $(document).ready(function(){
+	'use strict';
+
+	// Global variables
+	var numToGuess;
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -12,6 +16,51 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	// Create a new game
+  	$(".new").click(function(){
+  		newGame();
+  	})
+
+	// Reset for a new game
+	function newGame(){
+		// Number to guess
+		numToGuess = Math.round(Math.random()*100);
+
+		// Reset text and count
+		$("#feedback").empty().append("Make your Guess!");
+		$("#count").empty().append("0");
+	};
+
+	// Create new game on page load
+	newGame();
+
+	// Process a guess
+	$("#guessButton").submit(function(event){
+		// stop submit from refreshing the page
+		event.preventDefault();
+
+		var guess = $("#userGuess").val();
+		var diff = Math.abs(guess - numToGuess);
+
+		switch(true){
+			case (diff > 20):
+				$("#feedback").empty().append("Super cold!");
+				break;
+			case (diff <= 20):
+				$("#feedback").empty().append("Cold");
+				break;
+			case (diff <= 10):
+				$("#feedback").empty().append("Warm");
+				break;
+			case (diff <= 5):
+				$("#feedback").empty().append("Hot");
+				break;
+			case (diff <= 2):
+				$("#feedback").empty().append("Super hot");
+				break;
+			default:
+				$("#feedback").empty().append("Make your Guess!");
+				break;
+		};
+	});
 });
-
-
